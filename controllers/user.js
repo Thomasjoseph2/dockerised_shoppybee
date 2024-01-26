62c94c5dd114b10ebe2d5397a6c195642d3fa678
Thomas joseph <tj089502@gmail.com>
2024-01-26 10:44:57 +0530

    change

--- controllers/user.js
+++ controllers/user.js
@@ -44,7 +44,7 @@ const sendVerifyMail = async (name, email, userId) => {
 
       subject: "For verification mail",
 
-      html: '<p>Hi ' + name + ' ,please click here to <a href="https://eshaal.shop/verify?id=' + userId + '">Verify </a>  your mail.</p> '
+      html: '<p>Hi ' + name + ' ,please click here to <a href="https://shoppybee.eshaal.shop/verify?id=' + userId + '">Verify </a>  your mail.</p> '
 
     }
 
@@ -839,7 +839,7 @@ sendForgotPasswordMail = async (name, email, token) => {
 
       subject: "For Reset Password",
 
-      html: '<p>Hi ' + name + ' ,please click here to <a href="https://eshaal.shop/Change-password?token=' + token + '">Reset</a>  your  Password </p> '
+      html: '<p>Hi ' + name + ' ,please click here to <a href="https://shoppybee.eshaal.shop/Change-password?token=' + token + '">Reset</a>  your  Password </p> '
 
     }
 
@@ -1342,6 +1342,7 @@ const doPlaceOrder = async (req, res) => {
       }
 
     } else if (req.body['payment-method'] === 'ONLINE') {
+      console.log('here kjnlj lk,. ')
 
       const razorpayResponse = await userHelper.generateRazorpay(response.insertedId, total);
 
